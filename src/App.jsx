1e8f4df7199e5bc8932911fad1a9511cd51b2a36
Farhad Sultanov <sultanovv991@gmail.com>
2024-08-08 15:49:40 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react'
+import Slider from './components/Slider/slider'
+
+function App() {
+
+  return (
+    <>
+      <Slider/>
+    </>
+  )
+}
+
+export default App;
