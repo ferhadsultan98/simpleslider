a5b98619f9df75b1c297fc73fc0a236e2c82d4d0
Farhad Sultanov <sultanovv991@gmail.com>
2024-08-09 17:11:11 +0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import React from 'react'
 import Slider from './components/Slider/slider'
+import Example from './components/Example/example';
 
 function App() {
 
   return (
     <>
       <Slider/>
+      <Example/>
     </>
   )
 }
---
+++ src/components/Example/example.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./example.css";
+
+const Example = () => {
+  return (
+    <div className="Bodys">
+      <div className="loader">
+        <div className="circle" />
+        
+        <div className="text">
+        <span className="letter">T</span>
+        <span className="letter">A</span>
+        <span className="letter">X</span>
+          <span className="letter">A</span>
+          <span className="letter">U</span>
+          <span className="letter">D</span>
+          <span className="letter">I</span>
+          <span className="letter">T</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Example;
